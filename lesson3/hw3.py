e98d1a3feae211fe552f7b6a1491ed5d7f9e8f8f
Oleksii <79475561+GRO1982@users.noreply.github.com>
2021-04-18 23:13:05 +0300

    Update hw3.py

--- lesson3/hw3.py
+++ lesson3/hw3.py
@@ -3,7 +3,6 @@ def fibonacci(n):
         return n
     return fibonacci(n - 1) + fibonacci(n - 2)
 
-print(fibonacci(10))
 if __name__ == '__main__':
     n = int(input("Input n: "))
     print(f"x(n) = {fibonacci(n)}")
